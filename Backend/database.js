1bfb32c74bac1c02af98c97a8c5d0355b7d36f20
Gireson91 <gareth.ireson@nationwide.co.uk>
2022-10-25 12:39:32 +0100

    Lunch Save

--- Backend/database.js
+++ Backend/database.js
@@ -1,17 +1,43 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/kids_tv_db", {
+mongoose.connect("mongodb://127.0.0.1:27017/kids_tv_db", {
     useNewUrlParser: true
-}, () => console.log("Connection successful"));
+}, (err) => err ? console.error(err) : console.log("Connection to Mongo DB successful"));
 
 const programmeSchema = new mongoose.Schema({
-    name: {
+    'Name': {
         type: String,
         require: true
-    }
+    },
+    'Description': {
+        type: String,
+        require: true,
+        maxLength: 300
+    },
+    'Genre': {
+        type: String,
+        default: 'Unknown'
+    },
+    'Channel': {
+        type: String,
+        default: 'Unknown'
+    },
+    'First Aired': {
+        type: String,
+        default: 'Unknown'
+    },
+    'Episode Length': {
+        type: String,
+        default: 'Unknown'
+    },
+    'Rating': {
+        type: Number,
+        require: true,
+        min: [-1, `Come on, it can't be that bad!`],
+        max: [10, `I know you like it, but please keep it up to 10`]
+    },
 });
 
 const programme = mongoose.model('programme', programmeSchema);
 
 module.exports = programme;
-// 1 sec
--- Backend/routes/programmes.js
+++ Backend/routes/programmes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const programme  = require('../database');
 
-router.get("/hello", (req, res) => {
-    res.send("Hello World!")
+router.get("/getAll", async (req, res) => {
+    try {
+        const result = await programme.find({})
+        .then(results => res.send(results))
+    } catch(err) {
+        return next(err);
+    }
 });
-
-// router.get("/get", (req, res) => programme.find({}).then(results => res.send(results)).catch(err => next(err)));
 
 router.post("/add", async (req, res, next) => {
     try {
@@ -16,6 +19,24 @@ router.post("/add", async (req, res, next) => {
     }
 });
 
+router.delete("/delete/:id", async (req, res, next) => {
+    try {
+        const result = await programme.findByIdAndDelete(req.params.id);
+        res.status(200).send(result);
+    } catch(err) {
+        return next(err);
+    }
+});
+
+router.put("/amend/:id", async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const newData = req.body;
+        const result = await programme.findByIdAndUpdate(id, newData, next);
+        res.status(201).send(result);
+    } catch(err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
-// it was t
